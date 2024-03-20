258f01835f4dd114d7a70f2721ed59ed7715f578
maguerin <marilou.guerin2@soprasteria.com>
2024-03-20 15:50:06 +0100

    accueil et redirection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { DiagnostiqueComponent } from './diagnostique/diagnostique.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material.module";
+import {AccueilComponent} from "./accueil/accueil.component";
+import {StatistiquesComponent} from "./statistiques/statistiques.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiagnostiqueComponent
+    DiagnostiqueComponent,
+    AccueilComponent,
+    StatistiquesComponent
   ],
   imports: [
     BrowserModule,
